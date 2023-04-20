16533163cf7103cc0c1279f2d5862a6c0f88b137
Austin Warren <austingw2020@gmail.com>
2023-04-19 23:21:59 -0700

    added dot product

--- HauntedJaunt/Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/Observer.cs
+++ HauntedJaunt/Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/Observer.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Observer : MonoBehaviour
 {
     public Transform player;
+    public Transform enemy;
     public GameEnding gameEnding;
+
+    public float dot;
+    public float view_angle = 0.5f; // .5f is 60 degrees (if player is within that 60 degree angle of view from the enemy (while inside the hitbox they'll be seen)
 
     bool m_IsPlayerInRange;
 
@@ -20,13 +24,21 @@ public class Observer : MonoBehaviour
             {
                 if(raycastHit.collider.transform == player)
                 {
-                    gameEnding.CaughtPlayer();
+                    Vector3 playerDirection = (player.position - enemy.position).normalized;
+
+                    Vector3 enemyForward = enemy.transform.forward;
+
+                    float dotProduct = Vector3.Dot(playerDirection, enemyForward);
+                    dot = dotProduct;
+                    if (dotProduct > view_angle)
+                    {
+                        gameEnding.CaughtPlayer();
+                    }
                 }
 
             }
         }
     }
-
 
     void OnTriggerEnter(Collider other)
     {
